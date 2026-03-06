8bda2bcfe3bb38db5af424953fcabee221882a83
EDITH5607 <aadharshajith5607@gmail.com>
2026-03-06 17:47:33 +0530

    created routes and router instance and return it

---
+++ cmd/api/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+
+	// this returns a type  *httprouter.Router but this struct contain a serverHTTP method to satisfy the http.Handler interface
+	return router
+}
