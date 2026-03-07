b76f03035c2c2c674836fb0e67978d4e1ae82998
EDITH5607 <aadharshajith5607@gmail.com>
2026-03-07 20:07:09 +0530

    custom json type error fn s replaced the httprouter text type return error fns

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,11 +9,17 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	// we are registor our methods to the httprouter because router itself send the reponse as plain text 
+	// so we replace that with our json error reponse methods
+	router.NotFound = http.HandlerFunc(app.notFoundReponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 
-	// this returns a type  *httprouter.Router but this struct contain a serverHTTP method to satisfy the http.Handler interface
+	// this returns a type  *httprouter.Router but this struct contain 
+	// a serverHTTP method to satisfy the http.Handler interface
 	return router
 }
