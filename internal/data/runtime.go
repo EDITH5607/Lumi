6910d23861a1ada788ae27f684edf0f3973d0b97
EDITH5607 <aadharshajith5607@gmail.com>
2026-03-17 21:53:01 +0530

    umarsheling logic for json reading

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -28,3 +32,40 @@ func (r Runtime) MarshalJSON()([]byte, error) {
 	
 	return []byte(quotesjsonvalue),nil
 } 
+
+
+//type UnMarshaler interface {
+// UnMarshalJSON() ([]byte, error)
+// }
+// If the type does satisfy the interface, then Go will call its UnMarshalJSON() method and use the
+// []byte slice that it returns as the encoded JSON value.
+// If the type doesn’t have a UnMarshalJSON() method, then Go will fall back to trying to decode
+// it to JSON based on its own internal set of rules.
+
+
+func (r *Runtime)UnmarshalJSON(jsonValue []byte) error {
+	// unqoute the json value "value" -> value
+	unquotedJSONValue,err := strconv.Unquote(string(jsonValue)) 
+	if err!=nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+
+	// parts is a []string so if is length is not 2 that means its not the runtime format and parts[1] is the min value
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	} 
+
+	// convert the integer32 with base 10  part[0] = "100" -> part[0] = 100 part[1] = "mins"
+	i , err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// type conversion to Runtime
+	*r = Runtime(i)
+
+	return  nil
+}
