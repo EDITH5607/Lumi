187a1f75b2d6ec90f737359866715bf066d0d171
EDITH5607 <aadharshajith5607@gmail.com>
2026-04-02 15:27:35 +0530

    list route implemented

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -203,3 +203,28 @@ func(app *application)deleteMovieHandler(w http.ResponseWriter, r *http.Request)
 
 
 }
+
+func (app *application) listMovies(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title string
+		Genres []string
+		data.Filters
+	}
+	v := validator.New()
+	qs := r.URL.Query()
+
+	input.Title = app.readString(qs, "title", "")
+	input.Genres = app.readCSV(qs,"genres",[]string{})
+	input.Filters.Page = app.readInt(qs, "page",1,v)
+	input.Filters.PageSize = app.readInt(qs, "page_size",20,v)
+	input.Filters.Sort = app.readString(qs, "sort","id")
+	input.Filters.SafeSortlist = []string{"id", "title", "runtime", "-id", "-title", "-runtime"}
+	if data.ValidateFilters(v,&input.Filters); !v.Valid() {
+		app.failedValidationResponse(w,r,v.Errors)
+		return
+	}
+	fmt.Fprintf(w,"%+v", input) // +v gives the struct with its member name like title:hello,...
+	//%v only gives the value
+
+
+}
