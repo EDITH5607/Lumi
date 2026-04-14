1a88128e1a98c16f63d996e44d7a654066e10438
EDITH5607 <aadharshajith5607@gmail.com>
2026-04-15 03:10:55 +0530

    user register route added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,11 +15,18 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthCheckHandler)
+
+	//movie routes
 	router.HandlerFunc(http.MethodGet, "/v1/movies",app.listMovies)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch,"/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+
+
+	// user routes
+	router.HandlerFunc(http.MethodPost, "/v1/users",app.RegisterUser)
 
 	// this returns a type  *httprouter.Router but this struct contain 
 	// a serverHTTP method to satisfy the http.Handler interface
