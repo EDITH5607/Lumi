cfbe46fc4716fcd50498668d2b9f21db0c7bb56a
EDITH5607 <aadharshajith5607@gmail.com>
2026-06-05 23:30:46 +0530

    activation route added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -30,6 +30,7 @@ func (app *application) routes() http.Handler {
 
 	// user routes
 	router.HandlerFunc(http.MethodPost, "/v1/users",app.RegisterUser)
+	router.HandlerFunc(http.MethodPut,"/v1/users/activated", app.ActivateUser)
 
 	// this returns a type  *httprouter.Router but this struct contain 
 	// a serverHTTP method to satisfy the http.Handler interface
