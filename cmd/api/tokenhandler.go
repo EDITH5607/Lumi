e668ec083e50053e66f93ac09180c4c95eda5bbf
EDITH5607 <aadharshajith5607@gmail.com>
2026-06-06 23:52:20 +0530

    token creation for user authentication

---
+++ cmd/api/tokenhandler.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"Green/internal/data"
+	"Green/internal/validator"
+	"errors"
+	"net/http"
+	"os/user"
+)
+
+func(app *application) createAuthenticationToken(w http.ResponseWriter, r *http.Request) {
+	var input struct{
+		Email string 	`json:"email"`
+		Password string	`json:"password"`
+	}
+
+	err := app.readJSON(w,r,&input)
+	if err!=nil {
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
+	v := validator.New()
+
+	data.ValidateEmail(v,input.Email)
+	data.ValidatePassword(v,input.Password)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w,r,v.Errors)
+		return
+	}
+
+	user, err := app.model.Users.GetByEmail(input.Email)
+	if err!=nil {
+		switch {
+		case errors.Is(err,data.ErrRecordNotFound):
+			// app.invalid
+		default:
+			app.serverErrorResponse(w,r,err)
+		}
+		return
+	}
+
+	match, err := user.Password.Matches(input.Password)
+	if err!=nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
+	
+
+}
