02557b4c6c63ed0fa8e6179ea869042e3978cef3
EDITH5607 <aadharshajith5607@gmail.com>
2026-06-10 17:33:26 +0530

    permission based middleware applied on movie related routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,11 +20,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthCheckHandler)
 
 	//movie routes , required activated user middleware added for permission purpose
-	router.HandlerFunc(http.MethodGet, "/v1/movies",app.requiredActivatedUser(app.listMovies))
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requiredActivatedUser(app.createMovieHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requiredActivatedUser(app.showMovieHandler))
-	router.HandlerFunc(http.MethodPatch,"/v1/movies/:id", app.requiredActivatedUser(app.updateMovieHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requiredActivatedUser(app.deleteMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies",app.requirePermission("movies:read",app.listMovies))
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission("movies:write",app.createMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requirePermission("movies:read",app.showMovieHandler))
+	router.HandlerFunc(http.MethodPatch,"/v1/movies/:id", app.requirePermission("movies:write",app.updateMovieHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requirePermission("movies:write",app.deleteMovieHandler))
 
 
 
