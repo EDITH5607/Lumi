25fc65daa84e0e5e70a7e379dcd1925bdab38298
EDITH5607 <aadharshajith5607@gmail.com>
2026-06-10 15:15:25 +0530

    Permission related struct and methods implemented

---
+++ internal/data/permissions.go
@@ -0,0 +1,69 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+type Permissions []string
+
+
+func (p Permissions) Include(code string) bool {
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (m PermissionModel) GetAllForUser(userID int64) (Permissions,error) {
+	query := `
+			SELECT permissions.code
+			FROM permissions
+			INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+			INNER JOIN users ON users_permissions.user_id = users.id
+			WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	// rows is just a pointer towards the data in the query result,it gives you control to access it,
+	//  use next() to get the next row data
+	rows, err := m.DB.QueryContext(ctx,query,userID)
+	if err!=nil {
+		return nil,err
+	}
+	defer rows.Close()
+
+
+	var permissions Permissions
+
+	for rows.Next() {
+
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err!=nil {
+			return nil,err
+		}
+
+		permissions = append(permissions, permission)
+
+	}
+	if err = rows.Err(); err!=nil {
+		return nil,err
+	}
+
+	return permissions,nil
+} 
+
+
